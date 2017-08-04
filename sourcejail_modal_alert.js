264abbec33a4d339d9fa7b962963eb144465003b
Ujah Chigozie peter <ujahchigozie@gmail.com>
2017-08-04 22:25:54 +0800

    Update sourcejail_modal_alert.js

--- sourcejail_modal_alert.js
+++ sourcejail_modal_alert.js
@@ -1,9 +1,9 @@
 function sourcejail_alert(modal_type,modal_title,modal_message){
 	var modal_image = '';
 	switch(modal_type){
-		case'popup': modal_image = ''; break;
-		case'alert': modal_image = ''; break;
-		case'prompt': modal_image = ''; break;
+		case'popup': modal_image = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i>'; break;
+		case'alert': modal_image = '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>'; break;
+		case'prompt': modal_image = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i>'; break;
 		default: break;
 	}
 var buildDom = '<div id="modal_popup" class="popup response-message">' +
@@ -15,10 +15,11 @@ var buildDom = '<div id="modal_popup" class="popup response-message">' +
 	'	  <h4 class="popup-title">' + modal_image + ' ' + modal_title + '</h4>' +
 	'	</div>' +
 	'	<div class="popup-body" style="color:#000;">' + modal_message + '</div>' +
-                ((modal_type == 'prompt') ? '<div><input type="text"/></div>' :'') +
-	'	<div class="popup-btns">' +
-	'	<span class="btn btn-xs btn-danger modalbtn" data-modalresponse="0">Cancel</span>' +
-	'	<span class="btn btn-xs btn-success modalbtn" data-modalresponse="1">Yes</span>' +
+	((modal_type == 'prompt') ? '<div><input hidden="text" id="promptvalue"/></div>' :'') +
+	'<div class="popup-btns">' +
+	((modal_type == 'alert') ? '<span class="btn btn-xs btn-success modalbtn" data-modalresponse="1">Ok</span>' :'') +
+	((modal_type == 'popup') ? '<span class="btn btn-xs btn-danger modalbtn" data-modalresponse="0">Cancel</span>' +
+	'<span class="btn btn-xs btn-success modalbtn" data-modalresponse="1">Yes</span>' :'') +
 	'	</div>' +
 	'  </div>' +
 	'</div>' +
